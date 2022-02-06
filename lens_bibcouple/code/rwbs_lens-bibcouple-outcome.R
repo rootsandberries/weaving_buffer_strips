20f5ae364b60e56eb1b9de39a542149158317811
Sarah Young <sarahy@andrew.cmu.edu>
2022-02-05 21:04:50 -0500

    Outcome visualization with api data
    Adding code and plot for outcome cluster characterization using API data

--- lens_bibcouple/code/rwbs_lens-bibcouple-outcome.R
+++ lens_bibcouple/code/rwbs_lens-bibcouple-outcome.R
@@ -1,8 +1,8 @@
 #Analysis of Outcome Measure by Bibliographic Coupling Cluster
 
 #Install packages
-install.packages("fuzzyjoin")
-install.packages("here")
+#install.packages("fuzzyjoin")
+#install.packages("here")
 
 #Load libraries
 library(tidyverse)
@@ -31,7 +31,7 @@ myoutcomes_unique <- distinct_at(myoutcomes, vars("Title"))
 
 #Fuzzy join of clusters with outcome dataset on title (adjust distance until total records are found)
 mydata <- stringdist_inner_join(myclusters_bibx, myoutcomes_unique, by = "Title", max_dist = 5,
-                        distance_col = "distance")
+                                distance_col = "distance")
 
 mydata <- merge(myclusters_bibx, myoutcomes, by=c("Title"))
 mydatadf <- as.data.frame(mydata)
@@ -57,13 +57,74 @@ plot <- ggplot(outcome_by_cluster[order(outcome_by_cluster$n, decreasing = T),],
   theme(axis.text.x = element_text(angle = 65, size = 12), axis.text.y = element_text(size = 12)) +
   guides(fill = guide_legend(reverse = TRUE)) +
   theme(legend.title = element_text(size = 12), legend.text = element_text(size = 10)) #+
-  #scale_fill_manual(values = c("Biodiversity" = "darkseagreen", "Ecosystem Functioning" = "darkolivegreen4", "Human Use" = "darkslategray3", "Social" = "deepskyblue2", "Recreation" = "dodgerblue4", "Pollution" = "darkorange", "Soil Chemistry" = "burlywood3", "Soil Physical" = "chocolate4", "Other" = "gray"))
+#scale_fill_manual(values = c("Biodiversity" = "darkseagreen", "Ecosystem Functioning" = "darkolivegreen4", "Human Use" = "darkslategray3", "Social" = "deepskyblue2", "Recreation" = "dodgerblue4", "Pollution" = "darkorange", "Soil Chemistry" = "burlywood3", "Soil Physical" = "chocolate4", "Other" = "gray"))
 
 ggsave(plot, filename = "rwbs_outcome_plot.png", width = 7, height = 5, units = "in" )
 
 
+#Analysis of Outcome Measure by Bibliographic Coupling Cluster Using Lens API data
 
 
+#Load libraries
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(fuzzyjoin)
+library(here)
+
+
+#Import data
+myclusters_api <- read.csv(here("data", "20220205_clusters-louv_lens_api.csv"))
+myoutcomes <- read.csv(here("data", "13750_2018_126_MOESM6_ESM-outcome.csv"))
+
+load("./data/LENS_dataframe.RData") #loads record_df data object
+record_df_data.bib <- record_df %>% select(data.lens_id, data.title)
+dim(record_df_data.bib)
+names(record_df_data.bib)
+
+#Merge cluster to main dataset on bibliometrix name
+myclusters_api <- rename(myclusters_api, lensID = V1, cluster = V2)
+record_df_data.bib <- rename(record_df_data.bib, lensID = data.lens_id, Title = data.title)
+myclusters_bibx <- merge(myclusters_api, record_df_data.bib, by=c("lensID"))
+
+#Trim whitespace and make title lowercase in both cluster and outcome datasets
+myclusters_bibx$Title <- trimws(tolower(myclusters_bibx$Title))
+myoutcomes$Title <- trimws(tolower(myoutcomes$Title))
+
+#Remove duplicates from outcomes dataset
+myoutcomes_unique <- distinct_at(myoutcomes, vars("Title"))
+
+#Fuzzy join of clusters with outcome dataset on title (adjust distance until total records are found)
+mydata <- stringdist_inner_join(myclusters_bibx, myoutcomes_unique, by = "Title", max_dist = 5,
+                                distance_col = "distance")
+
+mydata <- merge(myclusters_bibx, myoutcomes, by=c("Title"))
+mydatadf <- as.data.frame(mydata)
+
+#Split into multiple rows by title and outcome
+mydata_sep <- separate_rows(mydatadf, Outcomes, sep=";")
+
+#Keep only unique rows to remove duplicate outcomes
+mydata_unique <- distinct_all(mydata_sep)
+
+#Group by and tally outcomes by clusters
+outcome_by_cluster <- mydata_unique %>%
+  group_by(Outcomes, cluster) %>%
+  tally()
+
+plot <- ggplot(outcome_by_cluster[order(outcome_by_cluster$n, decreasing = T),], aes(x=cluster, y=n, fill=factor(Outcomes, levels=c("Other", "Soil Physical", "Soil Chemistry", "Pollution", "Recreation", "Social", "Human Use", "Ecosystem Functioning", "Biodiversity")))) + 
+  geom_bar(position="fill", stat="identity") +
+  labs(x = "Cluster", y = "", fill = "Outcome") +
+  coord_flip() +
+  scale_y_continuous(labels=scales::percent) +
+  #scale_x_discrete(labels = wrap_format(13)) +
+  theme_bw(base_size = 12) +
+  theme(axis.text.x = element_text(angle = 65, size = 12), axis.text.y = element_text(size = 12)) +
+  guides(fill = guide_legend(reverse = TRUE)) +
+  theme(legend.title = element_text(size = 12), legend.text = element_text(size = 10)) #+
+#scale_fill_manual(values = c("Biodiversity" = "darkseagreen", "Ecosystem Functioning" = "darkolivegreen4", "Human Use" = "darkslategray3", "Social" = "deepskyblue2", "Recreation" = "dodgerblue4", "Pollution" = "darkorange", "Soil Chemistry" = "burlywood3", "Soil Physical" = "chocolate4", "Other" = "gray"))
+
+ggsave(plot, filename = "rwbs_outcome_plot_api.png", width = 7, height = 5, units = "in" )
 
 
 
